4a13eebcf314d5d2bac0d1241ebdb0cbf75b3616
skyclouds2001 <skyclouds2001@163.com>
2022-08-17 18:30:48 +0800

    feat: add axios

---
+++ monitor-system/src/lib/request.ts
@@ -0,0 +1,20 @@
+import axios, {AxiosRequestConfig, AxiosResponse} from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost',
+  timeout: 5000,
+})
+
+instance.interceptors.request.use(function (config: AxiosRequestConfig) {
+  return config;
+}, function (error: any) {
+  return Promise.reject(error);
+});
+
+instance.interceptors.response.use(function (response: AxiosResponse) {
+  return response;
+}, function (error: any) {
+  return Promise.reject(error);
+});
+
+export default instance
