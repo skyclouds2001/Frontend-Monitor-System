0358683396103cc5caca742ab96bdfe4753b36a5
skyclouds2001 <skyclouds2001@163.com>
2022-08-24 20:42:19 +0800

    feat: remove unused file

--- monitor-system/src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import IndexDBWrapper from 'indexdbwrapper';
-import type DBWrapper from 'indexdbwrapper';
-
-const db: DBWrapper = new IndexDBWrapper('monitor-system', 1);
-db.open();
-
-export default db;
