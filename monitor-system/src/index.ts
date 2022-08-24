33240a66bb4a999b0fc858a414f7c533a8c9d52c
skyclouds2001 <skyclouds2001@163.com>
2022-08-24 20:03:14 +0800

    refactor: fix test file

--- monitor-system/src/index.ts
+++ monitor-system/src/index.ts
@@ -1,8 +1,8 @@
 'use strict';
 
+import test from '@/lib/test'
 import env from '@/src/handleEnv';
 import initExceptionMonitor from '@/src/handleException';
-import initPerformanceMonitor from '@/src/handlePerformance';
 
 window.addEventListener('load', function MonitorSystem () {
   'use strict';
@@ -14,13 +14,6 @@ window.addEventListener('load', function MonitorSystem () {
   console.log(env);
 
   initExceptionMonitor();
-  initPerformanceMonitor();
-
-  async function test () {
-    // document.body.innerHTML = '<img src="1a" />';
-    // eval('var 1');
-    // await instance.get('/34')
-  }
 
   test();
 
