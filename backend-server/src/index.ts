3751f4861550b20210a84c6951607a70a621c7c9
skyclouds2001 <skyclouds2001@163.com>
2022-08-25 18:26:35 +0800

    feat: add post records API

--- backend-server/src/index.ts
+++ backend-server/src/index.ts
@@ -7,7 +7,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+/**
+ * 登录
+ */
 app.get('/v1/system/login', (req, res) => {
+  res.contentType('application/json');
+
   const { code } = req.query;
   const timestamp = new Date().getTime().toString();
   const buffer = Buffer.from(JSON.stringify({ code, timestamp }), 'utf-8');
@@ -26,20 +31,55 @@ app.get('/v1/system/login', (req, res) => {
       message: '请求失败',
     });
   }
+
   res.end();
 });
 
+/**
+ * 接受记录并缓存
+ */
 app.post('/v1/data/post', (req, res) => {
-  console.log(req.body);
+  res.contentType('application/json');
 
-  res.send();
+  const { type, data } = req.body;
+
+  if([0, 1].includes(type) && ['object', 'string'].includes(typeof data)) {
+
+    if (type === 0) {
+      cache.appInfo = { ...data };
+    } else {
+      cache.records.push({
+        timestamp: new Date().getTime(),
+        record: data,
+      });
+    }
+
+    res.send({
+      success: true,
+      data: null,
+      message: '请求成功',
+    });
+  } else {
+    res.send({
+      success: false,
+      data: null,
+      message: '请求失败',
+    });
+  }
+
   res.end();
 });
 
+/**
+ * 接受记录请求并返回缓存
+ */
 app.get('v1/data/get', (req, res) => {
+  res.contentType('application/json');
+
   console.log(req.params);
 
   res.send();
+
   res.end();
 });
 
@@ -49,9 +89,7 @@ app.listen(8080, () => {
 
 const Cache = class Cache {
   public appInfo: object = {};
-  public data: object[] = [];
+  public records: object[] = [];
 };
 
 const cache = new Cache();
-
-console.log(cache);
