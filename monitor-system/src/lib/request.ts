a9892b5512b044626d287450e2bf714897160779
skyclouds2001 <skyclouds2001@163.com>
2022-08-25 20:12:14 +0800

    feat: update host & request flag

--- monitor-system/src/lib/request.ts
+++ monitor-system/src/lib/request.ts
@@ -1,21 +1,21 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost',
+  baseURL: 'http://localhost:8080',
   timeout: 5000,
 })
 
 instance.interceptors.request.use(function (config: AxiosRequestConfig) {
   return config;
 }, function (error: any) {
-  error.isWatch = false;
+  error.isUnWatch = true;
   return Promise.reject(error);
 });
 
 instance.interceptors.response.use(function (response: AxiosResponse) {
   return response;
 }, function (error: any) {
-  error.isWatch = false;
+  error.isUnWatch = true;
   return Promise.reject(error);
 });
 
