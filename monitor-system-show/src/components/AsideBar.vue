c3ffddc6899ae12e1ab8c20fee6c32e56645c517
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 13:55:36 +0800

    feat: add AsideBar

---
+++ monitor-system-show/src/components/AsideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-menu router>
+    <el-menu-item index="1">异常监控</el-menu-item>
+    <el-menu-item index="2">关键性能数据监控</el-menu-item>
+    <el-menu-item index="3">用户行为数据监控</el-menu-item>
+    <el-menu-item index="4">HTTP请求监控</el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AsideBar',
+});
+</script>
+
+<style scoped>
+
+</style>
