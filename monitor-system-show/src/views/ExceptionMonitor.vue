915ade3f43def0ca70510ea2be24e883572cdb96
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 17:15:39 +0800

    fix: update label

--- monitor-system-show/src/views/ExceptionMonitor.vue
+++ monitor-system-show/src/views/ExceptionMonitor.vue
@@ -5,7 +5,7 @@
     </div>
     <el-table :data="records" style="width: 1000px;" stripe border highlight-current-row current-row-key="timeStamp">
       <el-table-column prop="type" label="类型" width="200" />
-      <el-table-column prop="message" label="信息" width="400" />
+      <el-table-column prop="message" label="描述" width="400" />
       <el-table-column prop="filename" label="文件名" width="200" />
       <el-table-column prop="lineno" label="行号" width="100" />
       <el-table-column prop="colno" label="列号" width="100" />
@@ -18,18 +18,16 @@ import { ref } from 'vue';
 import { useStore } from 'vuex'
 import { getRecords } from '@/api/getRecords';
 
-const store = useStore();
+useStore().commit('updateTitle','异常监控');
 
 const records = ref<object[]>([]);
 
 const init = async () => {
-  store.commit('updateTitle','异常监控');
   try {
     const result = await getRecords();
 
     if (result.success) {
       records.value = result.data.map((v: any) => v.record) as object[];
-      console.log(records.value);
     }
   } catch (error) {
     console.error(error);
