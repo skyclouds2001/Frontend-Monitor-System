333b13932957c2215521a4c320b9bcf156a4f97f
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 14:11:34 +0800

    feat: init views

---
+++ monitor-system-show/src/views/MainMonitor.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainMonitor',
+});
+</script>
+
+<style scoped lang="less">
+</style>
