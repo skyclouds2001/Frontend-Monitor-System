907ee0fc9b43b76f6b2c5353d51411ee383ad166
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 13:26:34 +0800

    feat: init project

---
+++ monitor-system-show/README.md
@@ -0,0 +1,8 @@
+# monitor-show-system
+
+### 技术栈
+
+* git
+* yarn
+* vite
+* typescript
---
+++ monitor-system-show/src/store/index.ts
@@ -0,0 +1,9 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state () {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  strict:  process.env.NODE_ENV !== 'production',
+});
