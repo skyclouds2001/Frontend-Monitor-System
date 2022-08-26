7984100b65f0369500ee1bb757a03445869af3cd
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 17:25:55 +0800

    feat: update back of page header

--- monitor-system-show/src/components/PageHeader.vue
+++ monitor-system-show/src/components/PageHeader.vue
@@ -1,14 +1,21 @@
 <template>
-  <el-page-header class="header" title="返回" :content="title" />
+  <el-page-header class="header" title="返回" :content="title" @back="back" />
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue';
+import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
+
+const router = useRouter();
 
 const store = useStore();
 
 const title = computed(() => store.state.pageHeaderTitle);
+
+const back = () => {
+  router.push('/')
+};
 </script>
 
 <script lang="ts">
