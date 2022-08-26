cb6bd88161c7780633f290cc4d30169f66ddac35
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 14:11:43 +0800

    feat: init route

--- monitor-system-show/src/router/index.ts
+++ monitor-system-show/src/router/index.ts
@@ -2,5 +2,26 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export default createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/MainMonitor.vue'),
+    },
+    {
+      path: '/monitor/exception',
+      component: () => import('@/views/ExceptionMonitor.vue'),
+    },
+    {
+      path: '/monitor/performance',
+      component: () => import('@/views/PerformanceMonitor.vue'),
+    },
+    {
+      path: '/monitor/behavior',
+      component: () => import('@/views/BehaviorMonitor.vue'),
+    },
+    {
+      path: '/monitor/request',
+      component: () => import('@/views/RequestMonitor.vue'),
+    },
+  ],
 });
