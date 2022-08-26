ab118aef2830ff2daefa81f0a772b7e64883f798
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 16:49:19 +0800

    feat: add js error table

--- monitor-system-show/src/views/ExceptionMonitor.vue
+++ monitor-system-show/src/views/ExceptionMonitor.vue
@@ -1,7 +1,38 @@
 <template>
+  <div>
+    <div>
+      <span style="font-weight: bold; font-size: 32px;">JS ERROR</span>
+    </div>
+    <el-table :data="records" style="width: 1000px;" stripe border highlight-current-row current-row-key="timeStamp">
+      <el-table-column prop="type" label="类型" width="200" />
+      <el-table-column prop="message" label="信息" width="400" />
+      <el-table-column prop="filename" label="文件名" width="200" />
+      <el-table-column prop="lineno" label="行号" width="100" />
+      <el-table-column prop="colno" label="列号" width="100" />
+    </el-table>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { getRecords } from '@/api/getRecords';
+
+const records = ref<object[]>([]);
+
+const init = async () => {
+  try {
+    const result = await getRecords();
+
+    if (result.success) {
+      records.value = result.data.map((v: any) => v.record) as object[];
+      console.log(records.value);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+init();
 </script>
 
 <script lang="ts">
