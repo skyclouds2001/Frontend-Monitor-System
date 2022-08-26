aeda972f7183acc6a804bf8da0d27b3522927ae7
skyclouds2001 <skyclouds2001@163.com>
2022-08-26 13:55:16 +0800

    feat: update App component

--- monitor-system-show/src/App.vue
+++ monitor-system-show/src/App.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import AsideBar from '@/components/AsideBar.vue';
 </script>
 
 <template>
-  <router-view></router-view>
+  <el-container>
+    <el-aside width="300px">
+      <AsideBar />
+    </el-aside>
+    <el-main>
+      <el-container>
+        <el-header height="60px"></el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        <el-footer height="60px"></el-footer>
+      </el-container>
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped>
